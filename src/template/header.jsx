71c8a231ad133a850a5ed4a943565249bbdbd009
Luiz Note <luflax34@gmail.com>
2019-09-18 16:16:13 -0300

    Adicionado menu responsivo

--- src/template/header.jsx
+++ src/template/header.jsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {withRouter} from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faBars} from '@fortawesome/free-solid-svg-icons';
 import './header.css'
 
 import HeaderButton from './headerButton'
@@ -9,6 +11,7 @@ import Logo from '../images/logo.png'
 const Header = props => {
     const [verticalPosition, setVerticalPosition] = useState(0)
     const [actualPage, setActualPage] = useState('')
+    const [isMenuCompacted, setIsMenuCompacted] = useState(true)
 
     function handleScroll() {
         setVerticalPosition(window.pageYOffset)
@@ -32,7 +35,7 @@ const Header = props => {
             <a href="#">
                 <img src={Logo} alt="Logo" width='80px' height='60px'/>
             </a>
-            <ul>
+            <ul className={isMenuCompacted ? 'compacted' : ''}>
                 <HeaderButton label='REEL' path='/' active={actualPage} 
                     onClick={e => handleButtonClick(e, '')}/>
                 <HeaderButton label='MOTIONS' path='/motions' active={actualPage} 
@@ -46,6 +49,9 @@ const Header = props => {
                 <HeaderButton label='ADMIN' path='/admin' active={actualPage} 
                     onClick={e => handleButtonClick(e, '/admin')}/>
             </ul>
+            <button className='compactBtn' onClick={e => setIsMenuCompacted(!isMenuCompacted)}>
+                <FontAwesomeIcon icon={faBars} size='lg'/>
+            </button>
         </nav>
         : 
         <></>
